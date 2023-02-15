7a3127ad650eef6f9b7acdfa256be8d738117aea
sallykhazaal <sallykhazaal@yahoo.com>
2023-02-15 13:44:08 -0500

    Completed Pinball Challenge

--- src/FirstAttemptindex.js
+++ src/FirstAttemptindex.js
@@ -1,4 +1,4 @@
-  let gameList = document.querySelector("#game-list");
+let gameList = document.querySelector("#game-list");
 
 fetch("http://localhost:3000/games")
   .then((res) => res.json())
--- src/index.js
+++ src/index.js
@@ -38,16 +38,17 @@ newForm.addEventListener("submit", (e) => {
   highScore.textContent = e.target["score-input"].value;
   e.target.reset(); //this will reset the form to 0 when you toggle to another game
   currentGame.high_score = e.target["score-input"].value;
-  // console.log(e.target["score-input"].value);
-  //then grab those inputs from the form
-  //and them somewhere
-  //because we are submitting on the newForm, the newForm will be our target, so when we do e.target we are getting the element target (with the submission infor we need from the form) from the e object that we have available to us whenever we are doing an event
-  //when we do e.target it shows you two inputs the submit input and the text input, use the idea of the text input to target it, do that with e.target["score-input"]
-  //we then are grabbing the html tag input with the score input id but we need its value, so we add .value
-
-  //newHighScore.textContent = e.target["score-input"].value;
-  //currentGames.high_score = e.target["score-input"].value;
 });
+
+// console.log(e.target["score-input"].value);
+//then grab those inputs from the form
+//and them somewhere
+//because we are submitting on the newForm, the newForm will be our target, so when we do e.target we are getting the element target (with the submission infor we need from the form) from the e object that we have available to us whenever we are doing an event
+//when we do e.target it shows you two inputs the submit input and the text input, use the idea of the text input to target it, do that with e.target["score-input"]
+//we then are grabbing the html tag input with the score input id but we need its value, so we add .value
+
+//newHighScore.textContent = e.target["score-input"].value;
+//currentGames.high_score = e.target["score-input"].value;
 
 //bill's correct answer below
 /*form.addEventListener("submit", (e) => {
